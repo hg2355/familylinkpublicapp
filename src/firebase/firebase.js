da8c8db53b36625e52bd6a7f610ee2eb3596635e
hg2355 <hg2355@columbia.edu>
2018-07-29 11:43:27 -0700

    added firebase info

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+    apiKey: "AIzaSyCFT8d4pV7l-Thb8CgaWkMqKoBAC7p9roo",
+    authDomain: "familylink-1532712793973.firebaseapp.com",
+    databaseURL: "https://familylink-1532712793973.firebaseio.com",
+    projectId: "familylink-1532712793973",
+    storageBucket: "familylink-1532712793973.appspot.com",
+    messagingSenderId: "788113778619"
 };
 
 firebase.initializeApp(config);
