cec0b8931fc0ebcbe3f62d446cd2a9ef25e80874
hg2355 <hg2355@columbia.edu>
2018-07-30 11:02:54 -0700

    added new background for king school

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,8 +1,37 @@
 import React from 'react';
 
 const DashboardPage = () => (
-  <div>
-    Dashboard page content
+    
+  <div className="container">
+  <div className="row">
+  <h2>Scheduled Fall Updates</h2>
+		<div className="timeline">
+            <span className="period period-active">Sept 16</span>  
+            <span className="period">Sept 30</span>  
+            <span className="period">Oct 18</span>
+            <span className="period">Nov 4</span>  
+            <span className="period">Nov 18</span>  
+            <span className="period">Dec 16</span>  
+		</div>
+  </div>
+
+  <div className="row">
+  <h2>Scheduled Spring Updates</h2>
+		<div className="timeline">
+            <span className="period">Jan 13</span>  
+            <span className="period">Jan 27</span>  
+            <span className="period">Feb 10</span>
+            <span className="period">Feb 24</span>  
+            <span className="period">Mar 10</span>  
+            <span className="period">Mar 24</span>  
+            <span className="period">Apr 14</span>  
+            <span className="period">May 5</span>  
+            <span className="period">May 19</span> 
+		</div>
+  </div>
+
+    
+
   </div>
 );
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ export const Header = ({ startLogout }) => (
     <div className="content-container">
       <div className="header__content">
         <Link className="header__title" to="/dashboard">
-          <h1>Boilerplate</h1>
+          <h1><img src="/images/school.gif" height="118" width="186" /></h1>
         </Link>
         <button className="button button--link" onClick={startLogout}>Logout</button>
       </div>
