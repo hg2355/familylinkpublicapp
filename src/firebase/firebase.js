17bcee753dfce13c2ab99ba663769bfeb91f0cb5
hg2355 <hg2355@columbia.edu>
2018-07-29 17:41:34 -0700

    final deployment of initial app without firebase info

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,7 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-
+    apiKey: "AIzaSyCFT8d4pV7l-Thb8CgaWkMqKoBAC7p9roo",
+    authDomain: "familylink-1532712793973.firebaseapp.com",
+    databaseURL: "https://familylink-1532712793973.firebaseio.com",
+    projectId: "familylink-1532712793973",
+    storageBucket: "familylink-1532712793973.appspot.com",
+    messagingSenderId: "788113778619"
 };
 
 firebase.initializeApp(config);
